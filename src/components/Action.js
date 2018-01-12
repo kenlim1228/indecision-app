64d6d36812e664894a3c9ba24e67d4bafeb82528
Ken Lim <ken.lim@live.ca>
2018-01-11 19:38:42 -0800

    Refactor to use shorthand when returning stateless component

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-const Action = (props) => {
-    return (
-        <div>
-            <button onClick={props.handlePick} disabled={!props.hasOptions}>
-                What to do?
-            </button>
-        </div>
-    );
-};
+const Action = (props) => (
+    <div>
+        <button onClick={props.handlePick} disabled={!props.hasOptions}>
+            What to do?
+        </button>
+    </div>
+);
 
 export default Action;
